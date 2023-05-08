ecc493259f2d4b96116c2030bfe246caf1bcf1be
nicol zarankin <nicolzarankin@gmail.com>
2023-05-08 19:17:32 +0300

    finished the structs file for now

--- TriviaProject/TriviaProject/Structs.h
+++ TriviaProject/TriviaProject/Structs.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <ctime>
+#include "IRequestHandler.h"
 
 using std::string;
+using std::vector;
 
 struct ErrorResponse
 {
@@ -16,4 +20,30 @@ struct LoginResponse
 struct SignupResponse
 {
 	unsigned int status;
+};
+
+struct LoginRequest
+{
+	string username;
+	string password;
+};
+
+struct SignupRequest
+{
+	string username;
+	string password;
+	string email;
+};
+
+struct RequestInfo
+{
+	int id;
+	time_t receivalTime;
+	vector<unsigned char> buffer;
+};
+
+struct RequestResult
+{
+	vector<unsigned char> response;
+	IRequestHandler* newHandler;
 };
