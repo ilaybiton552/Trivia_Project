b47f73efe006e006a978c39d0cc86d178137b16c
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-09 14:34:24 +0300

    Fixed all of the bugs First bug was circular reference, Structs included IRequestHandler and IReauestHandler included Structs In LoginRequestHandler, needed to write the function on cpp file in order to run the project (will be changed in the future)

--- TriviaProject/TriviaProject/LoginRequestHandler.h
+++ TriviaProject/TriviaProject/LoginRequestHandler.h
@@ -5,7 +5,7 @@
 class LoginRequestHandler : public IRequestHandler
 {
 public:
-	virtual bool isRequestRelevant(RequestInfo requestInfo);
-	virtual RequestResult handleRequest(RequestInfo requestInfo);
+	virtual bool isRequestRelevant(RequestInfo requestInfo) override;
+	virtual RequestResult handleRequest(RequestInfo requestInfo) override;
 };
 
