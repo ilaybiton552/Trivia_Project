e4f067e269fe8a32fd80e41e65857a29dd16f4af
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-09 18:13:00 +0300

    Finish method of converting used right shift, returns vector of bytes (the method works for every unsigned int number)

--- TriviaProject/TriviaProject/JsonResponsePacketSerializer.cpp
+++ TriviaProject/TriviaProject/JsonResponsePacketSerializer.cpp
@@ -36,9 +36,16 @@ vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(SignupResp
 /// <summary>
 /// Converts a number to a byte
 /// </summary>
-/// <param name="num">int, the number to convert</param>
-/// <returns>unsigned char, the converted number</returns>
-unsigned char JsonResponsePacketSerializer::convertNumberToByte(const int num)
+/// <param name="num">unsigned int, the number to convert</param>
+/// <returns>vector of bytes, the converted number</returns>
+vector<unsigned char> JsonResponsePacketSerializer::convertNumberToByte(const unsigned int num)
 {
-    return 0;
+    vector<unsigned char> numberInBytes;
+    for (int i = 0; i < MAX_BYTES_UNSIGNED_INT; i++)
+    {
+        // >> binary right shift, & binary and
+        numberInBytes.push_back((num >> NUM_OF_BITS_IN_BYTE * i) & MAX_VALUE_OF_BYTE); 
+    }
+    std::reverse(numberInBytes.begin(), numberInBytes.end());
+    return numberInBytes;
 }
