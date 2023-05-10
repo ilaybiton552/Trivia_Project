739b7d05095b17e16746872f6e266ab132ae6224
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-10 21:23:55 +0300

    Implemented isRequestRelevant Changed the name of the method (const and added reference) TODO: add helper class which will have all of the random methods (such as converting byte to number) Change the method convertByteToNumber - the method will get from index and len to calculate the number from every index (then don't need to create a new vector for the number to calculate)

--- TriviaProject/TriviaProject/LoginRequestHandler.cpp
+++ TriviaProject/TriviaProject/LoginRequestHandler.cpp
@@ -1,11 +1,37 @@
 #include "LoginRequestHandler.h"
 
-bool LoginRequestHandler::isRequestRelevant(RequestInfo requestInfo)
+/// <summary>
+/// Checks if the request is relevant
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the information of the request</param>
+/// <returns>bool, if the request is relevant to the handler</returns>
+bool LoginRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
 {
-	return false;
+    vector<unsigned char> codeRequestByte;
+    codeRequestByte.push_back(requestInfo.buffer[0]);
+    int codeRequest = convertByteToNumber(codeRequestByte);
+	return (codeRequest == LOGIN_REQUEST || codeRequest == SIGNUP_REQUEST);
 }
 
-RequestResult LoginRequestHandler::handleRequest(RequestInfo requestInfo)
+RequestResult LoginRequestHandler::handleRequest(const RequestInfo& requestInfo)
 {
 	return RequestResult();
+}
+
+
+/// <summary>
+/// the function converts bytes to integer
+/// </summary>
+/// <param name="buffer">the bytes to convert</param>
+/// <returns>the converted bytes as number</returns>
+unsigned int LoginRequestHandler::convertByteToNumber(const vector<unsigned char>& buffer)
+{
+    int num = 0;
+
+    for (int i = 0; i < MAX_BYTES_UNSIGNED_INT; i++) // from bytes to int
+    {
+        num = (num << NUM_OF_BITS_IN_BYTE) + buffer[i];
+    }
+
+    return num;
 }
--- TriviaProject/TriviaProject/LoginRequestHandler.h
+++ TriviaProject/TriviaProject/LoginRequestHandler.h
@@ -2,10 +2,17 @@
 #include "IRequestHandler.h"
 #include "Structs.h"
 
+#define NUM_OF_BITS_IN_BYTE 8
+#define MAX_BYTES_UNSIGNED_INT 4
+#define LOGIN_REQUEST 101
+#define SIGNUP_REQUEST 102
+
 class LoginRequestHandler : public IRequestHandler
 {
 public:
-	virtual bool isRequestRelevant(RequestInfo requestInfo) override;
-	virtual RequestResult handleRequest(RequestInfo requestInfo) override;
+	virtual bool isRequestRelevant(const RequestInfo& requestInfo) override;
+	virtual RequestResult handleRequest(const RequestInfo& requestInfo) override;
+private:
+	static unsigned int convertByteToNumber(const vector<unsigned char>& buffer);
 };
 
