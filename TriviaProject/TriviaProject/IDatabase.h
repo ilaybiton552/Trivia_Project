236f18ff4552a76518b1e10e9aa2684d33b3050d
nicol zarankin <nicolzarankin@gmail.com>
2023-05-11 15:13:28 +0300

    Created the open and close functions and added a helper function

--- TriviaProject/TriviaProject/IDatabase.h
+++ TriviaProject/TriviaProject/IDatabase.h
@@ -9,9 +9,9 @@ class IDatabase
 public:
 	virtual bool open() = 0;
 	virtual bool close() = 0;
-	virtual int doesUserExist(string username) = 0;
-	virtual int doesPasswordMatch(string username, string password) = 0;
-	virtual int addNewUser(string username, string password, string email) = 0;
+	virtual int doesUserExist(const string username) = 0;
+	virtual int doesPasswordMatch(const string username, const string password) = 0;
+	virtual int addNewUser(const string username, const string password, const string email) = 0;
 
 };
 
--- TriviaProject/TriviaProject/SqliteDatabase.cpp
+++ TriviaProject/TriviaProject/SqliteDatabase.cpp
@@ -1,26 +1,72 @@
 #include "SqliteDatabase.h"
 
+/// <summary>
+/// the function opens database
+/// </summary>
+/// <returns>if the database has opened successfully</returns>
 bool SqliteDatabase::open()
 {
+	sqlite3* db;
+	string dbFileName = "Trivia_DataBase.sqlite";
+	int file_exist = _access(dbFileName.c_str(), 0);
+	int res = sqlite3_open(dbFileName.c_str(), &db); // open the database
+	if (res != SQLITE_OK) // if error has occured
+	{
+		db = nullptr;
+		cout << "Failed to open DataBase" << endl;
+		return false;
+	}
 
+	if (file_exist != 0) // if the database doesn't exist
+	{
+		sqlQuery(db, "CREATE TABLE IF NOT EXISTS Users(USERNAME TEXT PRIMARY KEY, PASSWORD TEXT, EMAIL TEXT);");
+	}
+
+	this->m_database = db;
+	return true;
 }
 
+/// <summary>
+/// the funciton closes the database
+/// </summary>
+/// <returns>if the database has closed successfully</returns>
 bool SqliteDatabase::close()
 {
-
+	sqlite3_close(this->m_database);
+	this->m_database = nullptr;
+	return true;
 }
 
-int SqliteDatabase::doesUserExist(string username)
+int SqliteDatabase::doesUserExist(const string username)
 {
 
 }
 
-int SqliteDatabase::doesPasswordMatch(string username, string password)
+int SqliteDatabase::doesPasswordMatch(const string username, const string password)
 {
 
 }
 
-int SqliteDatabase::addNewUser(string username, string password, string email)
+int SqliteDatabase::addNewUser(const string username, const string password, const string email)
 {
 
+}
+
+/// <summary>
+/// the function does a sql query
+/// </summary>
+/// <param name="db">the database</param>
+/// <param name="sqlStatement">the query that we want to be done</param>
+/// <returns>if the query comleted successfully</returns>
+bool SqliteDatabase::sqlQuery(sqlite3* db, const char* sqlStatement)
+{
+	char** errMessage = nullptr;
+	int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, errMessage); // does the query
+	if (res != SQLITE_OK) // if an error has occured
+	{
+		cout << "Error has occurred: " << errMessage << endl;
+		return false;
+	}
+
+	return true;
 }
