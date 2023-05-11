87474144bb7d843f58abf987077aa28d7071aca3
nicol zarankin <nicolzarankin@gmail.com>
2023-05-11 15:13:28 +0300

    Created the open and close functions and added a helper function

--- TriviaProject/TriviaProject/SqliteDatabase.h
+++ TriviaProject/TriviaProject/SqliteDatabase.h
@@ -1,13 +1,29 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <io.h>
 #include "IDatabase.h"
+#include "sqlite3.h"
+
+using std::cout;
+using std::endl;
+using std::string;
 
 class SqliteDatabase : public IDatabase
 {
 public:
+	//Methods
 	virtual bool open() override;
 	virtual bool close() override;
-	virtual int doesUserExist(string username) override;
-	virtual int doesPasswordMatch(string username, string password) override;
-	virtual int addNewUser(string username, string password, string email) override;
+	virtual int doesUserExist(const string username) override;
+	virtual int doesPasswordMatch(const string username, const string password) override;
+	virtual int addNewUser(const string username, const string password, const string email) override;
+
+private:
+	//Field
+	sqlite3* m_database;
+
+	//Method
+	bool sqlQuery(sqlite3* db, const char* sqlStatement);
 };
 
