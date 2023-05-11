4ccb165ca2544fec6b1de098a15bcf56ea65998d
nicol zarankin <nicolzarankin@gmail.com>
2023-05-11 23:08:27 +0300

    Implemented the loggedUser function(c'tor and getUserName)

--- TriviaProject/TriviaProject/LoggedUser.cpp
+++ TriviaProject/TriviaProject/LoggedUser.cpp
@@ -1,1 +1,19 @@
 #include "LoggedUser.h"
+
+/// <summary>
+/// constructor
+/// </summary>
+/// <param name="username">the logged username</param>
+LoggedUser::LoggedUser(const string username)
+{
+	this->m_username = username;
+}
+
+/// <summary>
+/// the function returns the username
+/// </summary>
+/// <returns>the username</returns>
+string LoggedUser::getUsername() const
+{
+	return this->m_username;
+}
--- TriviaProject/TriviaProject/LoggedUser.h
+++ TriviaProject/TriviaProject/LoggedUser.h
@@ -6,6 +6,8 @@ using std::string;
 class LoggedUser
 {
 public:
+	//Methods
+	LoggedUser(const string username); // c'tor
 	//Getter
 	string getUsername() const;
 
