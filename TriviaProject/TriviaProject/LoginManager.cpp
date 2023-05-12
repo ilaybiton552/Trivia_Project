302febcea7563489d86974124930944d3225517a
nicol zarankin <nicolzarankin@gmail.com>
2023-05-12 12:45:28 +0300

    Changed the returned value from the functions in the loginManager class from void to int (to return if there was an error)

--- TriviaProject/TriviaProject/LoginManager.cpp
+++ TriviaProject/TriviaProject/LoginManager.cpp
@@ -1,1 +1,16 @@
 #include "LoginManager.h"
+
+int signup(const string username, const string password, const string email)
+{
+
+}
+
+int login(const string username, const string password)
+{
+
+}
+
+int logout(const string username)
+{
+
+}
