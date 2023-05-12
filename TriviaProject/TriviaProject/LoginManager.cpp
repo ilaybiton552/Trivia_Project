fbd40f46a94664fda84b53a731691ed4501b1e6d
nicol zarankin <nicolzarankin@gmail.com>
2023-05-12 14:05:00 +0300

    Wrote all the LoginManager class functions and added c'tor, d'tor and findLoggedUser(the function returns pointer to logged user) functions

--- TriviaProject/TriviaProject/LoginManager.cpp
+++ TriviaProject/TriviaProject/LoginManager.cpp
@@ -1,16 +1,98 @@
 #include "LoginManager.h"
 
-int signup(const string username, const string password, const string email)
+/// <summary>
+/// constructor
+/// </summary>
+LoginManager::LoginManager()
 {
-
+	this->m_database->open();
 }
 
-int login(const string username, const string password)
+/// <summary>
+/// destructor
+/// </summary>
+LoginManager::~LoginManager()
 {
-
+	this->m_database->close();
+	this->m_loggedUsers.clear();
 }
 
-int logout(const string username)
+/// <summary>
+/// the function signups the user
+/// </summary>
+/// <param name="username">the username</param>
+/// <param name="password">the password</param>
+/// <param name="email">the email</param>
+/// <returns>the result code</returns>
+int LoginManager::signup(const string username, const string password, const string email)
 {
+	if (m_database->doesUserExist(username)) // check if the username doesn't exist in the database
+	{
+		return USER_ALREADY_EXIST_CODE;
+	}
 
+	m_database->addNewUser(username, password, email); // add new user to the database
+	m_loggedUsers.push_back(LoggedUser(username)); // add the user to the logged user list
+	return SUCCESS_CODE;
+}
+
+/// <summary>
+/// the function logins the user
+/// </summary>
+/// <param name="username">the username</param>
+/// <param name="password">the password</param>
+/// <returns>the result code</returns>
+int LoginManager::login(const string username, const string password)
+{
+	if (!m_database->doesUserExist(username)) // check if the user exists in the database
+	{
+		return USER_DOES_NOT_EXIST_CODE;
+	}
+	else if (!m_database->doesPasswordMatch(username, password)) // check if the password doesn't match the username
+	{
+		return WRONG_PASSWORD_CODE;
+	}
+	else if (findLoggedUser(username) != nullptr) // check if the user already logged
+	{
+		return USER_ALREADY_LOGGED_CODE;
+	}
+
+	m_loggedUsers.push_back(LoggedUser(username)); // add the user to the logged user list
+	return SUCCESS_CODE;
+}
+
+/// <summary>
+/// the function logouts the user
+/// </summary>
+/// <param name="username">the username</param>
+/// <returns>the result code</returns>
+int LoginManager::logout(const string username)
+{
+	LoggedUser* userToLogout = findLoggedUser(username);
+
+	if (userToLogout == nullptr) // check if the user doen't logged
+	{
+		return USER_DOES_NOT_LOGGED_CODE;
+	}
+
+	remove(m_loggedUsers.begin(), m_loggedUsers.end(), *userToLogout); // remove the user from the logged user list
+	return SUCCESS_CODE;
+}
+
+/// <summary>
+/// the function return pointer to logged user
+/// </summary>
+/// <param name="username">the logged user username to find</param>
+/// <returns>pointer to logged user</returns>
+LoggedUser* LoginManager::findLoggedUser(const string username) const
+{
+	for (auto loggedUser : m_loggedUsers)
+	{
+		if (loggedUser.getUsername() == username)
+		{
+			return &loggedUser;
+		}
+	}
+
+	return nullptr;
 }
--- TriviaProject/TriviaProject/LoginManager.h
+++ TriviaProject/TriviaProject/LoginManager.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "IDatabase.h"
 #include "LoggedUser.h"
+
+enum loginCodes {SUCCESS_CODE, USER_DOES_NOT_EXIST_CODE, USER_ALREADY_EXIST_CODE, WRONG_PASSWORD_CODE, USER_ALREADY_LOGGED_CODE, USER_DOES_NOT_LOGGED_CODE};
 
 using std::vector;
 using std::string;
@@ -11,6 +14,8 @@ class LoginManager
 {
 public:
 	//Methods
+	LoginManager(); // c'tor
+	~LoginManager(); // d'tor
 	int signup(const string username, const string password, const string email);
 	int login(const string username, const string password);
 	int logout(const string username);
@@ -19,5 +24,8 @@ private:
 	//Fields
 	IDatabase* m_database;
 	vector<LoggedUser> m_loggedUsers;
+
+	//Method
+	LoggedUser* findLoggedUser(const string username) const;
 };
 
