74a9283cc6ce14f9847369c7f1ba8310a5fcf1c4
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-13 17:30:45 +0300

    Added new class RequestHandlerFactory

---
+++ TriviaProject/TriviaProject/RequestHandlerFactory.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "LoginManager.h"
+#include "LoginRequestHandler.h"
+
+class RequestHandlerFactory
+{
+public:
+	LoginRequestHandler* createLoginRequestHandler() const;
+	LoginManager& getLoginManager();
+
+private:
+	LoginManager m_loginManager;
+	IDatabase* m_database;
+};
+
