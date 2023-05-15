223eeede5239ac3f8567b9302dbf3db18ea12e45
nicol zarankin <nicolzarankin@gmail.com>
2023-05-15 20:11:21 +0300

    Finished to write the UserInfoCheck function and changed the function signatures to static

--- TriviaProject/TriviaProject/UserInfoCheck.h
+++ TriviaProject/TriviaProject/UserInfoCheck.h
@@ -5,6 +5,8 @@
 using std::string;
 using std::regex;
 using std::regex_match;
+using std::regex_search;
+using std::smatch;
 
 class UserInfoCheck
 {
@@ -12,10 +14,10 @@ public:
 	static bool checkUserInfo(const string password, const string email, const string address, const string phone_number, const string birthdate);
 
 private:
-	bool passwordCheck(const string password);
-	bool emailCheck(const string email);
-	bool addressCheck(const string address);
-	bool phoneNumberCheck(const string phone_number);
-	bool birthdateCheck(const string birthdate);
+	static bool passwordCheck(const string password);
+	static bool emailCheck(const string email);
+	static bool addressCheck(const string address);
+	static bool phoneNumberCheck(const string phoneNumber);
+	static bool birthdateCheck(const string birthdate);
 };
 
