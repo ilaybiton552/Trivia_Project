914ad8db3b81a1ccbc58256523ab01e4abcbd428
nicol zarankin <nicolzarankin@gmail.com>
2023-05-15 18:13:53 +0300

    Added the data check in the signup and expand the user information

--- client.py
+++ client.py
@@ -1,28 +1,120 @@
 import socket
 import re
+import json
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8826
 RECV = 1024
+LOGIN_REQUEST_CODE = 101
+SIGNUP_REQUEST_CODE = 102
+DISCONNECT_REQUEST_CODE = 0
+LOGIN = 1
+SIGNUP = 2
+EXIT = 0
+CUSTOM_MESSAGE = 3
 
 
 def client_socket():
     """
-    Opens a client socket and talks with the server
+    Opens a client socket
+    :return: Socket sock
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # open the socket
+    server_address = (SERVER_IP, SERVER_PORT)
+    sock.connect(server_address)  # connects to the server
+    return sock
+
+
+def login_message():
+    """
+    the function receives login information
+    :return: the login request
+    """
+    username = input("Please enter your username: ")
+    password = input("Please enter the password: ")
+    login_request = {"username": username, "password": password}
+    login_request = json.dumps(login_request)
+    return login_request
+
+
+def signup_message():
+    """
+    the function receives signup information
+    :return: the signup request
+    """
+    username = input("Please enter your username: ")
+
+    password = input("Please enter the password: ")
+    while not (password_check(password)):
+        print("The password does not match the format")
+        print("The format for password: at least 1 digit, 1 uppercase letter, 1 lowercase letter and 1 special char")
+        password = input("Please enter the password: ")
+
+    email = input("Please enter your email: ")
+    while not (email_check(email)):
+        print("The email does not match the format")
+        email = input("Please enter your email: ")
+
+    address = input("Please enter your address: ")
+    while not (address_check(address)):
+        print("The address does not match the format")
+        print("The format for address: X, X, X")
+        address = input("Please enter your address: ")
+
+    phone_number = input("Please enter your phone number: ")
+    while not (phone_number_check(phone_number)):
+        print("The phone number does not match the format")
+        print("The format for phone number: 0XXXXXXXX or 05XXXXXXXX")
+        phone_number = input("Please enter your phone number: ")
+
+    birthdate = input("Please enter your birthdate: ")
+    while not (birthdate_check(birthdate)):
+        print("The birthdate does not match the format")
+        print("The format for birthdate: DD.MM.YYYY")
+        birthdate = input("Please enter your birthdate: ")
+
+    signup_request = {"username": username, "password": password, "email": email, "address": address, "phone_number": phone_number, "birthdate": birthdate}
+    signup_request = json.dumps(signup_request)
+    return signup_request
+
+def serialize_message(message, code, sock):
+    """
+    the function creates and sends encode message to the server
+    :param message: the message to send
+    :param code: the request / response code
+    :param sock: the socket with server
     :return: None
     """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (SERVER_IP, SERVER_PORT)
-    sock.connect(server_address)
+    data_length = len(message)
+    message = code.to_bytes(1, 'big') + data_length.to_bytes(4, 'big') + message.encode()
+    sock.sendall(message)
 
-    server_msg = sock.recv(RECV).decode()
-    print("Server message: ", server_msg)
 
-    client_msg = input("Enter the message for the server: ")
-    print(client_msg)
-    if (client_msg == "Hello"):
-        sock.sendall(client_msg.encode())
-    sock.close()
+def deserialize_message(message):
+    """
+    the function deserialize the message from the server
+    :param message: the message that the server sends
+    :return: None
+    """
+    code = message[0]
+    data_length = int.from_bytes(message[1:5], "big", signed=False)
+    data = message[5:-1].decode('utf-8') + '}'
+
+    print("Server's response: ")
+    print("Message code: " + str(code))
+    print("Data length: " + str(data_length))
+    print("Message content: " + data)
+
+
+def custom_message():
+    """
+    The function gets a custom message from the client
+    :return: tuple, the data and the code of the message
+    """
+    code = int(input("Enter the code of the message: "))
+    data = input("Enter the data of the message: ")
+    return data, code
+
 
 def password_check(password):
     """
@@ -45,6 +137,7 @@ def password_check(password):
     else:
         return True
 
+
 def email_check(email):
     """
     the function checks if the email is valid or not
@@ -59,6 +152,7 @@ def email_check(email):
     else:
         return False
 
+
 def address_check(address):
     """
     the function checks if the address is valid or not
@@ -72,6 +166,7 @@ def address_check(address):
         return True
     else:
         return False
+
 
 def phone_number_check(phone_number):
     """
@@ -88,27 +183,74 @@ def phone_number_check(phone_number):
     else:
         return False
 
-def birth_date_check(birth_date):
+
+def birthdate_check(birthdate):
     """
-    the function checks if the birth date format is valid or not
-    :param birth_date: the birth date to check
+    the function checks if the birthdate format is valid or not
+    :param birthdate: the birthdate to check
     :type: string
     :return: if the birth date format is valid or not
     :rtype: bool
     """
     regex = re.compile(r'([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})')
-    if(re.fullmatch(regex, birth_date)):
+    if(re.fullmatch(regex, birthdate)):
         return True
     else:
         return False
 
+
+def menu():
+    """
+    the function prints the menu and receives user choice
+    :return: user's choice
+    """
+    print("What would you like to do?")
+    print("0 - Exit")
+    print("1 - Login")
+    print("2 - Signup")
+    print("3 - Custom message")
+    choice = -1
+    while not (0 <= choice <= 3):
+        try:
+            choice = int(input("Please enter your choice: "))
+        except Exception as ex:
+            print("Invalid input!" + str(ex))
+
+    return choice
+
+
+def action(choice, sock):
+    """
+    the function does the wanted action
+    :param choice: user's choice of action
+    :param sock: Socket sock
+    :return: None
+    """
+    if choice is LOGIN:
+        serialize_message(login_message(), LOGIN_REQUEST_CODE, sock)
+    elif choice is SIGNUP:
+        serialize_message(signup_message(), SIGNUP_REQUEST_CODE, sock)
+    elif choice is EXIT:
+        print("Goodbye!")
+        serialize_message("disconnect", DISCONNECT_REQUEST_CODE, sock)
+    elif choice is CUSTOM_MESSAGE:
+        data, code = custom_message()
+        serialize_message(data, code, sock)
+
+
 def main():
     try:
-        while True:
-            print(birth_date_check(input("Enter birth date: ")))
+        sock = client_socket()
+        user_choice = menu()
+        while (user_choice != EXIT):
+            action(user_choice, sock)
+            deserialize_message(sock.recv(RECV))
+            user_choice = menu()
+        action(user_choice, sock)
+        sock.close()
     except Exception as exc:
         print("The exception is: ", exc)
-    input() # press any key to exit
+    input()  # press any key to exit
 
 
 if __name__ == "__main__":
