123dfba46fd2a10b738140f99fb9ca3865fcb44a
nicol zarankin <nicolzarankin@gmail.com>
2023-05-21 21:52:42 +0300

    Finished to write the function signatures in the Question class

--- TriviaProject/TriviaProject/Question.h
+++ TriviaProject/TriviaProject/Question.h
@@ -1,5 +1,21 @@
 #pragma once
+#include <string>
+#include <vector>
+
+using std::string; 
+using std::vector;
+
 class Question
 {
+public:
+	//Methods
+	Question(const string question, const string correctAnswer, const string inccorrectAnswer1, const string inccorrectAnswer2, const string inccorrectAnswer3);
+	string getQuestion() const;
+	vector<string> getPossibleAnswers() const;
+	int getCorrectAnswerId() const;
+private:
+	string m_question;
+	string m_possibleAnswers;
+	int correctAnswerId;
 };
 
