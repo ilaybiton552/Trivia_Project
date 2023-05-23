0fe72b36c818611940c332063653aa2a5aa10e89
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-23 21:25:27 +0300

    Finish method handleRequest

--- TriviaProject/TriviaProject/MenuRequestHandler.cpp
+++ TriviaProject/TriviaProject/MenuRequestHandler.cpp
@@ -22,7 +22,77 @@ bool MenuRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
     return (requestInfo.id <= CREATE_ROOM_CODE && requestInfo.id >= SIGNOUT_CODE);
 }
 
+/// <summary>
+/// Handles the request
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the information of the request</param>
+/// <returns>RequestResult, the result for the user's request</returns>
 RequestResult MenuRequestHandler::handleRequest(const RequestInfo& requestInfo)
+{
+    if (requestInfo.id == SIGNOUT_CODE)
+    {
+        return signout(requestInfo);
+    }
+
+    if (requestInfo.id == GET_ROOM_CODE)
+    {
+        return getRooms(requestInfo);
+    }
+
+    if (requestInfo.id == GET_PLAYERS_CODE)
+    {
+        return getPlayersInRoom(requestInfo);
+    }
+
+    if (requestInfo.id == GET_STATS_CODE)
+    {
+        return getPersonalStats(requestInfo);
+    }
+
+    if (requestInfo.id == GET_HIGH_SCORE_CODE)
+    {
+        return getHighScore(requestInfo);
+    }
+
+    if (requestInfo.id == JOIN_ROOM_CODE)
+    {
+        return joinRoom(requestInfo);
+    }
+
+    return createRoom(requestInfo);
+}
+
+RequestResult MenuRequestHandler::signout(const RequestInfo request)
+{
+    return RequestResult();
+}
+
+RequestResult MenuRequestHandler::getRooms(const RequestInfo request)
+{
+    return RequestResult();
+}
+
+RequestResult MenuRequestHandler::getPlayersInRoom(const RequestInfo request)
+{
+    return RequestResult();
+}
+
+RequestResult MenuRequestHandler::getPersonalStats(const RequestInfo request)
+{
+    return RequestResult();
+}
+
+RequestResult MenuRequestHandler::getHighScore(const RequestInfo request)
+{
+    return RequestResult();
+}
+
+RequestResult MenuRequestHandler::joinRoom(const RequestInfo request)
+{
+    return RequestResult();
+}
+
+RequestResult MenuRequestHandler::createRoom(const RequestInfo request)
 {
     return RequestResult();
 }
