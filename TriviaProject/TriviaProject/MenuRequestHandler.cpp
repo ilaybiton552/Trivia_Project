18136c3acd78bdce7cbcf841890aadcae18a8859
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-23 21:17:24 +0300

    Finish constructor for MenuRequestHandler

--- TriviaProject/TriviaProject/MenuRequestHandler.cpp
+++ TriviaProject/TriviaProject/MenuRequestHandler.cpp
@@ -1,5 +1,17 @@
 #include "MenuRequestHandler.h"
 
+/// <summary>
+/// Constructor of MenuRequestHandler
+/// </summary>
+/// <param name="user">LoggedUser, the user that logged</param>
+/// <param name="roomManager">reference of RoomManager, the room manager</param>
+/// <param name="statisticsManager">reference of StatisticsManager, the statistics manager</param>
+/// <param name="handlerFactory">reference of RequestHandlerFactory, the handler factory</param>
+MenuRequestHandler::MenuRequestHandler(const LoggedUser user, RoomManager& roomManager, StatisticsManager& statisticsManager, RequestHandlerFactory& handlerFactory) : 
+    m_user(user), m_roomManager(roomManager), m_statisticsManager(statisticsManager), m_handlerFactory(handlerFactory)
+{
+}
+
 bool MenuRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
 {
     return false;
