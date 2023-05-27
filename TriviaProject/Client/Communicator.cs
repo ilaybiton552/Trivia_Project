7e64e3cae9a6ea76d32816ed11f3122309ee919c
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-27 18:10:59 +0300

    Finish sending the server login request Finish sending and receiving messages from the client Create new class Communicator which will communicate with the server

---
+++ TriviaProject/Client/Communicator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Communicator
+    {
+        private TcpClient client;
+
+        public Communicator()
+        {
+            client = new TcpClient();
+        }
+    }
+}
--- TriviaProject/Client/LoginWindow.xaml.cs
+++ TriviaProject/Client/LoginWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,7 +37,31 @@ namespace Client
         /// </summary>
         private void LoginClick(object sender, RoutedEventArgs e)
         {
-            // TODO
+            TcpClient client = new TcpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8826);
+            client.Connect(serverEndPoint);
+            NetworkStream clientStream = client.GetStream();
+
+            string json = JsonConvert.SerializeObject(loginRequest);
+            byte[] jsonBuffer = new ASCIIEncoding().GetBytes(json);
+            byte[] length = BitConverter.GetBytes(jsonBuffer.Length);
+            Array.Reverse(length);
+            byte[] buffer = new byte[5 + jsonBuffer.Length];
+            buffer[0] = 101;
+            for (int i = 1; i < 5; i++)
+            {
+                buffer[i] = length[i - 1];
+            }
+            for (int i = 0; i < jsonBuffer.Length; i++)
+            {
+                buffer[i + 5] = jsonBuffer[i];
+            }
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+            buffer = new byte[4096];
+            int bytesRead = clientStream.Read(buffer, 0, 4096);
+            string message = Encoding.Default.GetString(buffer);
+            MessageBox.Show(message);
         }
 
         /// <summary>
