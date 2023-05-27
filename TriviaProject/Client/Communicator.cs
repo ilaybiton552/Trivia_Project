6853da528674c375933b9879e6999262b871d638
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-27 18:24:55 +0300

    Added new method to Commumicator Added method which closes the connection with the server

--- TriviaProject/Client/Communicator.cs
+++ TriviaProject/Client/Communicator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,25 @@ namespace Client
     class Communicator
     {
         private TcpClient client;
+        private NetworkStream clientStream;
+        private const int port = 8826;
+        private const string host = "127.0.0.1";
 
         public Communicator()
         {
             client = new TcpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(host), port);
+            client.Connect(serverEndPoint);
+            clientStream = client.GetStream();
+        }
+
+        /// <summary>
+        /// Closes the cliet connection
+        /// </summary>
+        public void Close()
+        {
+            clientStream.Close();
+            client.Close();
         }
     }
 }
