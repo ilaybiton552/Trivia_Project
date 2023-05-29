42b2d5a3483623bfc25df76c8ba681fc26e7279a
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-29 14:01:34 +0300

    Added new window StatisticsWindow Added buttons to the window - personal statistics and high scores Added button back - to get to menu window

---
+++ TriviaProject/Client/StatisticsWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for StatisticsWindow.xaml
+    /// </summary>
+    public partial class StatisticsWindow : Window
+    {
+        private Communicator communicator;
+        private string username;
+
+        public StatisticsWindow(ref Communicator communicator, string username)
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            this.communicator = communicator;
+            this.username = username;
+            usernameTextBlock.Text += username;
+        }
+
+        /// <summary>
+        /// Goes a window back (menu window)
+        /// </summary>
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
