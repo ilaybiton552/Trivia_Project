0ebde11d2233cb0cb29c4acccea1b564cdf7af04
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-29 14:47:07 +0300

    Added new window - HighScoreWindow

---
+++ TriviaProject/Client/HighScoreWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for HighScoreWindow.xaml
+    /// </summary>
+    public partial class HighScoreWindow : Window
+    {
+        private Communicator communicator;
+        private string username;
+
+        public HighScoreWindow(ref Communicator communicator, string username)
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            this.communicator = communicator;
+            this.username = username;
+        }
+    }
+}
