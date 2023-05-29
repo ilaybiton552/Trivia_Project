3bbfae8e4c6cc63816a21876ba6905748c18e876
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-29 12:33:29 +0300

    Added new window Added MenuWindow

---
+++ TriviaProject/Client/MenuWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        private Communicator communicator;
+        public MenuWindow(ref Communicator communicator)
+        {
+            InitializeComponent();
+            this.communicator = communicator;
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+    }
+}
