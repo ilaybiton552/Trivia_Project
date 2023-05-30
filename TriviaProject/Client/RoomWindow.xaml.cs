4fb18a798108b8b8120104da67c302920ede6d5e
ilay biton <ilaybitonminecraft@gmail.com>
2023-05-30 17:33:32 +0300

    Added new method in RoomWindow The method gets the list of players in the room and updates roomData

--- TriviaProject/Client/RoomWindow.xaml.cs
+++ TriviaProject/Client/RoomWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,10 @@ namespace Client
         private Communicator communicator;
         private string username;
         private RoomData roomData;
+        private const int GetPlayersRequestCode = 105;
+        private const int GetPlayersResponseCode = 205;
+
+
         public RoomWindow(ref Communicator communicator, string username, RoomData roomData)
         {
             InitializeComponent();
@@ -29,6 +34,30 @@ namespace Client
             this.communicator = communicator;
             this.username = username;
             this.roomData = roomData;
+            GetPlayers();
         }
+
+        /// <summary>
+        /// Gets the players in the room
+        /// </summary>
+        private void GetPlayers()
+        {
+            // sends packet to server
+            string json = JsonConvert.SerializeObject(new RoomIdRequest() { roomId = roomData.id });
+            PacketInfo clientPacket = new PacketInfo() { code = GetPlayersRequestCode, data = json };
+            communicator.SendPacket(clientPacket);
+
+            // gets message from server
+            PacketInfo serverPacket = communicator.GetMessageFromServer();
+            if (serverPacket.code != GetPlayersResponseCode)
+            {
+                MessageBox.Show("Error occured", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            GetPlayersResponse response = JsonConvert.DeserializeObject<GetPlayersResponse>(serverPacket.data);
+            roomData.players = response.players;
+        }
+
     }
 }
