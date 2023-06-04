128c6e6660de20dbea98f9cb63c007c8202d0ed1
ilay biton <ilaybitonminecraft@gmail.com>
2023-06-04 18:17:14 +0300

    Finish getRoomState in RoomMemberRequestHandler Used getRoomState method in Room class

--- TriviaProject/TriviaProject/RoomMemberRequestHandler.cpp
+++ TriviaProject/TriviaProject/RoomMemberRequestHandler.cpp
@@ -1,4 +1,5 @@
 #include "RoomMemberRequestHandler.h"
+#include "JsonResponsePacketSerializer.h"
 
 /// <summary>
 /// Constructor of RoomMemberRequestHandler
@@ -27,7 +28,17 @@ RequestResult RoomMemberRequestHandler::leaveRoom(RequestInfo requestInfo)
 
 }
 
+/// <summary>
+/// Gets the state of the room
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the information of the request</param>
+/// <returns>RequestResult, the result of the requst</returns>
 RequestResult RoomMemberRequestHandler::getRoomState(RequestInfo requestInfo)
 {
+	RequestResult result;
 
+	result.newHandler = nullptr;
+	result.response = JsonResponsePacketSerializer::serializeResponse(m_room.getRoomState());
+
+	return result;
 }
