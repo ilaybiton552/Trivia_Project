a7c8f832a5877a37c3fe64dbdf610f2be419f314
ilay biton <ilaybitonminecraft@gmail.com>
2023-06-17 09:32:29 +0300

    Finish handle request in GameRequestHandler Added documentation for all of the methods

--- TriviaProject/TriviaProject/GameRequestHandler.cpp
+++ TriviaProject/TriviaProject/GameRequestHandler.cpp
@@ -23,10 +23,71 @@ bool GameRequestHandler::isRequestRelevant(const RequestInfo& requestInfo)
 }
 
 /// <summary>
+/// Handlers the request of the client
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the request of the client</param>
+/// <returns>RequestResult, the result for the request</returns>
+RequestResult GameRequestHandler::handleRequest(const RequestInfo& requestInfo)
+{
+	switch (requestInfo.id)
+	{
+	case LEAVE_GAME_CODE:
+		return leaveGame(requestInfo);
+	case GET_QUESTION_CODE:
+		return getQuestion(requestInfo);
+	case SUBMIT_ANSWER_CODE:
+		return submitAnswer(requestInfo);
+	case GET_GAME_RESULT_CODE:
+		return getGameResults(requestInfo);
+	}
+	return RequestResult();
+}
+
+/// <summary>
 /// Getter for game
 /// </summary>
 /// <returns>Game, the game of the handler</returns>
 Game GameRequestHandler::getGame() const
 {
 	return m_game;
+}
+
+/// <summary>
+/// Gets the next question for the client
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the information of the request</param>
+/// <returns>RequestResult, the result for the request</returns>
+RequestResult GameRequestHandler::getQuestion(const RequestInfo& requestInfo)
+{
+	return RequestResult();
+}
+
+/// <summary>
+/// Submits the answer of the client
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the information of the request</param>
+/// <returns>RequestResult, the result for the request</returns>
+RequestResult GameRequestHandler::submitAnswer(const RequestInfo& requestInfo)
+{
+	return RequestResult();
+}
+
+/// <summary>
+/// Gets the results of the game
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the information of the request</param>
+/// <returns>RequestResult, the result for the request</returns>
+RequestResult GameRequestHandler::getGameResults(const RequestInfo& requestInfo)
+{
+	return RequestResult();
+}
+
+/// <summary>
+/// Leaves the game
+/// </summary>
+/// <param name="requestInfo">RequestInfo, the informtaion of the request</param>
+/// <returns>RequestResult, the result of the request</returns>
+RequestResult GameRequestHandler::leaveGame(const RequestInfo& requestInfo)
+{
+	return RequestResult();
 }
