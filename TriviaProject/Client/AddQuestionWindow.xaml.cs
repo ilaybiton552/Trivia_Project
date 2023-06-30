6dd536afcddd6b50f6877460326c13fb20dad66c
nicol zarankin <nicolzarankin@gmail.com>
2023-06-30 15:38:15 +0300

    Added the window for adding question

---
+++ TriviaProject/Client/AddQuestionWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for AddQuestionWindow.xaml
+    /// </summary>
+    public partial class AddQuestionWindow : Window
+    {
+        private Communicator communicator;
+        private string username;
+
+        public AddQuestionWindow(ref Communicator communicator, string username)
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            this.communicator = communicator;
+            this.username = username;
+        }
+
+        /// <summary>
+        /// Goes a window back (menu window)
+        /// </summary>
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            MenuWindow menuWindow = new MenuWindow(ref communicator, username);
+            Close();
+            menuWindow.ShowDialog();
+        }
+    }
+}
